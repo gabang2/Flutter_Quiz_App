b47a11e29c2487f746623de916e9269ad3cba8f0
gabang2 <offbeat1020@naver.com>
2022-09-07 11:40:49 +0700

    2강 완료(퀴즈 화면)

--- lib/screen/screen_quiz.dart
+++ lib/screen/screen_quiz.dart
@@ -51,10 +51,9 @@ class _QuizScreenState extends State<QuizScreen> {
   Widget _buildQuizCard(Quiz quiz, double width, double height) {
     return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(color: Colors.white),
-        color: Colors.white
-      ),
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Colors.white),
+          color: Colors.white),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
@@ -92,9 +91,21 @@ class _QuizScreenState extends State<QuizScreen> {
                 minWidth: width * 0.5,
                 height: height * 0.05,
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)
+                    borderRadius: BorderRadius.circular(10)),
+                child: ElevatedButton(
+                  child: _currentIndex == widget.quizs!.length - 1
+                      ? Text('결과 보기', style: TextStyle(color: Colors.white))
+                      : Text('다음 문제', style: TextStyle(color: Colors.white)),
+                  style: ElevatedButton.styleFrom(primary: Colors.deepPurple),
+                  onPressed: _answers[_currentIndex] == -1 ? null : () {
+                    if (_currentIndex == widget.quizs!.length) {
+                    } else {
+                      _answerState = [false, false, false, false];
+                      _currentIndex += 1;
+                      _controller.next();
+                    }
+                  },
                 ),
-                child: ElevatedButton(child: Text('버튼2'), onPressed: (){},),
               ),
             ),
           )
@@ -107,28 +118,25 @@ class _QuizScreenState extends State<QuizScreen> {
     List<Widget> _children = [];
     for (int i = 0; i < 4; i++) {
       _children.add(CandWidget(
-        index: i,
-        text: quiz.candidates![i],
-        width: width,
-        answerState: _answerState[i],
-        tap: (){
-          setState((){
-            for (int j=0; j<4; j++){
-              if (j == i) {
-                _answerState[j] = true;
-                _answers[_currentIndex] = j;
-                print(_answers[_currentIndex]);
-                print(width);
-              } else {
-                _answerState[j] = false;
+          index: i,
+          text: quiz.candidates![i],
+          width: width,
+          answerState: _answerState[i],
+          tap: () {
+            setState(() {
+              for (int j = 0; j < 4; j++) {
+                if (j == i) {
+                  _answerState[j] = true;
+                  _answers[_currentIndex] = j;
+                  print(_answers[_currentIndex]);
+                  print(width);
+                } else {
+                  _answerState[j] = false;
+                }
               }
-            }
-          });
-        }
-      ));
-      _children.add(
-        Padding(padding: EdgeInsets.all(width * 0.024))
-      );
+            });
+          }));
+      _children.add(Padding(padding: EdgeInsets.all(width * 0.024)));
     }
     return _children;
   }
